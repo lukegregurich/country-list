0b536c696a08b6254a92dcdcde80af6306b8cc68
Luke Gregurich <lgreguri@purdue.edu>
2018-05-16 14:53:10 -0400

    add js functionality, further visual updates in css

--- countries..js
+++ countries..js
@@ -12,3 +12,29 @@ const renderListItem = function(label, value){
   }
   return item
 }
+
+const renderList = function(data){
+    const list = document.createElement('ul')
+    Object.keys(data).map(function(label){
+        const item = renderListItem(label, data[label])
+        list.appendChild(item)
+    })
+    return list
+}
+
+const handleSubmit = function(ev){
+    ev.preventDefault() 
+    const form = ev.target
+    const country = {
+        countryName: form.countryName.value,
+        capital: form.capital.value,
+    }
+    const list = renderList(country)
+    const countries = document.querySelector('#countries')
+    countries.appendChild(list)
+   
+    form.reset();
+    form.countryName.focus()
+  }
+
+  form.addEventListener('submit', handleSubmit)
